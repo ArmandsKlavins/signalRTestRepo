f1384d7f8ed7a71a41344ad3fb26b8c2f82f1fbb
Armands Kļaviņš <armands.klavins@bbit.lv>
2018-08-05 20:15:50 +0300

    initial commit

---
+++ MVCChatTests/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCChatTests.Hubs;
+namespace MVCChatTests.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<int> staticList = new List<int>();
+        public ActionResult Index()
+        {
+            staticList.Add(1);
+            ViewBag.Count = staticList.Count;
+            Session["user"] = "User" + staticList.Count;
+            ViewBag.Check = true;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page." + Session["user"];
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCChatTests/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCChatTests.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMsg(string message, string name)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.Group("NewGroup").addNewMessageToPage(message, name);
+        }
+
+        public void Join()
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Groups.Add(Context.ConnectionId, "NewGroup");
+        }
+    }
+}
